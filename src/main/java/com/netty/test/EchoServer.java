0b3927298e52421fa883c4f0b336d1bb7317ed9b
ruirui.qu <ruirui.qu@qunar.com>
2020-03-08 14:45:51 +0800

    init

---
+++ src/main/java/com/netty/test/EchoServer.java
@@ -0,0 +1,18 @@
+package com.netty.test;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2020-03-08<p>
+//-------------------------------------------------------
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("==>");
+    }
+}
